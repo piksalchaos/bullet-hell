494ae5be8696159855fd072be2b924193382fd4f
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-09-22 23:11:31 -0700

    added a third round just cuz

--- scenes/round_manager.gd
+++ scenes/round_manager.gd
@@ -5,9 +5,9 @@ func begin() -> void:
 
 func begin_upcoming_round() -> void:
 	if get_child_count() == 0: return
-	var round: Round = get_child(0)
-	round.enemies_defeated.connect(goto_next_round)
-	round.begin()
+	var upcoming_round: Round = get_child(0)
+	upcoming_round.enemies_defeated.connect(goto_next_round)
+	upcoming_round.begin()
 
 func goto_next_round() -> void:
 	var previous_round = get_child(0)
