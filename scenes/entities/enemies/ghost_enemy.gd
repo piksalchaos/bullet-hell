f2167f67bdc72284d2b275c7ebae9b465475f0ec
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-10-06 15:36:06 -0700

    ghosts can now exit after some time

--- scenes/entities/enemies/ghost_enemy.gd
+++ scenes/entities/enemies/ghost_enemy.gd
@@ -1,25 +1,32 @@
 extends Node2D
 
 const GHOST_BULLET = preload("res://scenes/bullets/ghost_bullet.tscn")
-const TWEEN_DURATION := 0.75
+const TWEEN_ENTER_DURATION := 0.75
+const TWEEN_EXIT_DURATION := 1.5
 @onready var bullet_timer: Timer = $BulletTimer
+@onready var exit_timer: Timer = $ExitTimer
 @onready var color_component: ColorComponent = $ColorComponent
 
 @export var starting_position: Vector2
 @export var initial_color_id: GameProperties.COLOR_ID
+@export var exit_time: float = 8
 
 func _ready() -> void:
-	change_position(starting_position)
+	change_position(starting_position, begin_attacking, TWEEN_ENTER_DURATION)
 	color_component.color_id = initial_color_id
+	exit_timer.wait_time = exit_time
 
-func change_position(new_position: Vector2) -> void:
-	bullet_timer.stop()
+func change_position(
+	new_position: Vector2,
+	callback: Callable = func(): pass,
+	tween_duration: float = TWEEN_ENTER_DURATION,
+	ease_type: Tween.EaseType = Tween.EASE_OUT
+) -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", new_position, TWEEN_DURATION) \
-	 	 .set_ease(Tween.EASE_OUT) \
+	tween.tween_property(self, "position", new_position, tween_duration) \
+	 	 .set_ease(ease_type) \
 		 .set_trans(Tween.TRANS_SINE)
-	tween.tween_callback(begin_attacking)
-
+	tween.tween_callback(callback)
 	
 func begin_attacking() -> void:
 	bullet_timer.start()
@@ -32,3 +39,10 @@ func _on_bullet_timer_timeout() -> void:
 		bullet.rotation = (GameProperties.player_position - position).angle() + PI*0.1*(i - 1)
 		bullet.initial_color_id = GameProperties.COLOR_ID.RED if red_bullet_index == i else GameProperties.COLOR_ID.WHITE
 		GameProperties.bullet_container.add_child(bullet)
+
+func _on_exit_timer_timeout() -> void:
+	var new_position = Vector2(
+		-100 if GameProperties.STAGE_WIDTH - position.x > GameProperties.STAGE_WIDTH*0.5 else GameProperties.STAGE_WIDTH + 100,
+		position.y + 40
+	)
+	change_position(new_position, func(): queue_free(), TWEEN_EXIT_DURATION, Tween.EASE_IN)
