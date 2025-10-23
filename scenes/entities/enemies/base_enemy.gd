cc94f702f5014ed43702c4d19a4b75aca9f8896f
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-10-22 23:34:59 -0700

    working on tutorial + new orb enemy

--- scenes/entities/enemies/base_enemy.gd
+++ scenes/entities/enemies/base_enemy.gd
@@ -13,7 +13,8 @@ const TWEEN_EXIT_DURATION := 1.5
 func _ready() -> void:
 	change_position(starting_position, begin_attacking, TWEEN_ENTER_DURATION)
 	color_component.set_color_id(initial_color_id)
-	bullet_emitter.color_id = initial_color_id
+	if bullet_emitter:
+		bullet_emitter.color_id = initial_color_id
 
 func change_position(
 	new_position: Vector2,
