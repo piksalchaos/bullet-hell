04c3fa2796a398d525e5c6fe474222a32b0bddaa
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-10-24 02:21:25 -0700

    new animation for player getting hit

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -2,10 +2,15 @@ class_name Player extends Area2D
 
 @export var is_vulnerable := true
 
+@onready var bullet_absorber: Area2D = $BulletAbsorber
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var main_sprite: AnimatedSprite2D = $MainSprite
 @onready var shadow_sprite: AnimatedSprite2D = $ShadowSprite
+@onready var heart_sprite: Sprite2D = $HeartSprite
 @onready var hit_audio: AudioStreamPlayer = $HitAudio
+@onready var hit_cooldown_audio: AudioStreamPlayer = $HitCooldownAudio
+@onready var recover_audio: AudioStreamPlayer = $RecoverAudio
+@onready var hit_particles: CPUParticles2D = $HitParticles
 
 const PLAYER_BULLET = preload("res://scenes/bullets/player_bullet.tscn")
 const WIDTH := 16.0
@@ -39,19 +44,44 @@ func update_animation(velocity_x):
 	main_sprite.play(animation_name)
 	shadow_sprite.play("hit" if is_on_cooldown else animation_name)
 	if is_on_cooldown:
-		main_sprite.position.x = (randf()-0.5) * cooldown_timer.time_left * 10
+		main_sprite.position.x = (randf()-0.5) * cooldown_timer.time_left * 16
+		heart_sprite.position.x = (randf()-0.5) * cooldown_timer.time_left * 5
+		heart_sprite.position.y = (randf()-0.5) * cooldown_timer.time_left * 5
+		
 
 func hit() -> void:
 	if is_vulnerable:
 		got_hit.emit()
-	main_sprite.modulate.a = 0.6
-	shadow_sprite.self_modulate.a = 0.2
+	main_sprite.modulate.a = 0.4
+	shadow_sprite.self_modulate.a = 0.15
+	heart_sprite.modulate.a = 0.12
 	is_on_cooldown = true
 	cooldown_timer.start()
 	hit_audio.play()
+	hit_cooldown_audio.play()
+	var tween = create_tween()
+	tween.tween_property(bullet_absorber, "scale", Vector2(0.1, 0.1), 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	#bullet_absorber.modulate = Color(Color.WHITE, 0.8)
+	bullet_absorber.monitoring = false
+	hit_particles.emitting = true
 
 func _on_cooldown_timer_timeout() -> void:
+	var tween = create_tween()
+	tween.tween_property(bullet_absorber, "scale", Vector2(0.8, 0.8), 0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	tween.tween_callback(reset_after_hit)
+
+func reset_after_hit():
 	is_on_cooldown = false
+	bullet_absorber.show()
 	main_sprite.modulate.a = 1
 	shadow_sprite.self_modulate.a = 0.6
+	heart_sprite.modulate.a = 1
 	main_sprite.position.x = 0
+	heart_sprite.position.x = 0
+	bullet_absorber.monitoring = true
+	var tween = create_tween()
+	tween.set_parallel()
+	tween.tween_property(bullet_absorber, "scale", Vector2(1, 1), 0.25).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	tween.tween_property(bullet_absorber, "modulate", Color.WHITE, 0.25).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	hit_cooldown_audio.stop()
+	recover_audio.play()
